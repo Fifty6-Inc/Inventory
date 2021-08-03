284c3cdf8c091183ec571533a273bf3d17dc27b8
Mikael Weiss <Campingmikael@icloud.com>
2021-08-03 09:03:50 -0600

    Organized things a little, removed code I won't use

---
+++ Inventory/Presentation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inventory
+//
+//  Created by Mikael Weiss on 8/3/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Yeet")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
