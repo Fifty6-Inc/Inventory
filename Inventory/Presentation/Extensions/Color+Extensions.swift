d032d101059b2078077f025354d11c85cf7a5d8d
Mikael Weiss <Campingmikael@icloud.com>
2021-08-03 12:17:03 -0600

    Removed code I'm not using yet

--- Inventory/Presentation/Extensions/Color+Extensions.swift
+++ Inventory/Presentation/Extensions/Color+Extensions.swift
@@ -17,16 +17,3 @@ extension Color {
     static let appGray = Color("appGray")
     static let appDeepBlue = Color("appDeepBlue")
 }
-
-extension Color {
-    /// Example: Color(hex: 0xA10115)
-    init(hex: UInt, alpha: Double = 1) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: alpha
-        )
-    }
-}
