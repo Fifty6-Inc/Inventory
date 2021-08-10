7f71a457c65418e3908ff26c62702159ce025f28
Mikael Weiss <Campingmikael@icloud.com>
2021-08-10 16:24:12 -0600

    Updated CoreDataStoreTests

--- Persistence/CoreDataStoreTests.swift
+++ Persistence/CoreDataStoreTests.swift
@@ -39,10 +39,8 @@ class CoreDataStoreTests: XCTestCase {
         // Given
         _ = store.newItem()
         
-        // When
+        // When/Then
         XCTAssertNoThrow(try store.save())
-        
-        // Then
     }
     
     // MARK: - Delete All
@@ -126,6 +124,140 @@ class CoreDataStoreTests: XCTestCase {
         
         // When
         let entities = try store.getAllItems()
+        
+        XCTAssertEqual(entities.count, 3)
+    }
+    
+    // MARK: - Project
+    
+    func testNewProject() {
+        // When
+        let result = store.newProject()
+        
+        // Then
+        XCTAssertNotNil(result)
+    }
+    
+    func testGetProjectWithUnknownID() {
+        // Given
+        var result: Persistence.Project?
+        let randomID = UUID()
+        
+        // When
+        XCTAssertNoThrow(result = try store.getProject(with: randomID))
+        
+        // Then
+        XCTAssertNil(result)
+    }
+    
+    func testGetProjectWithKnownID() {
+        // Given
+        var result: Persistence.Project?
+        let id = UUID()
+        let newEntity = store.newProject()
+        newEntity.id = id
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        XCTAssertNoThrow(result = try store.getProject(with: id))
+        
+        // Then
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result?.id, id)
+    }
+    
+    func testDeleteProject() {
+        // Given
+        var result: Persistence.Project?
+        let id = UUID()
+        let newEntity = store.newProject()
+        newEntity.id = id
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        XCTAssertNoThrow(try store.deleteProject(with: id))
+        
+        // Then
+        XCTAssertNoThrow(result = try store.getProject(with: id))
+        XCTAssertNil(result)
+    }
+    
+    func testGetAllProjects() throws {
+        // Given
+        _ = store.newProject()
+        _ = store.newProject()
+        _ = store.newProject()
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        let entities = try store.getAllProjects()
+        
+        XCTAssertEqual(entities.count, 3)
+    }
+    
+    // MARK: - ProjectItem
+    
+    func testNewProjectItem() {
+        // When
+        let result = store.newProjectItem()
+        
+        // Then
+        XCTAssertNotNil(result)
+    }
+    
+    func testGetProjectItemWithUnknownID() {
+        // Given
+        var result: Persistence.ProjectItem?
+        let randomID = UUID()
+        
+        // When
+        XCTAssertNoThrow(result = try store.getProjectItem(with: randomID))
+        
+        // Then
+        XCTAssertNil(result)
+    }
+    
+    func testGetProjectItemWithKnownID() {
+        // Given
+        var result: Persistence.ProjectItem?
+        let id = UUID()
+        let newEntity = store.newProjectItem()
+        newEntity.id = id
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        XCTAssertNoThrow(result = try store.getProjectItem(with: id))
+        
+        // Then
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result?.id, id)
+    }
+    
+    func testDeleteProjectItem() {
+        // Given
+        var result: Persistence.ProjectItem?
+        let id = UUID()
+        let newEntity = store.newProjectItem()
+        newEntity.id = id
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        XCTAssertNoThrow(try store.deleteProjectItem(with: id))
+        
+        // Then
+        XCTAssertNoThrow(result = try store.getProjectItem(with: id))
+        XCTAssertNil(result)
+    }
+    
+    func testGetAllProjectItems() throws {
+        // Given
+        _ = store.newProjectItem()
+        _ = store.newProjectItem()
+        _ = store.newProjectItem()
+        XCTAssertNoThrow(try store.save())
+        
+        // When
+        let entities = try store.getAllProjectItems()
         
         XCTAssertEqual(entities.count, 3)
     }
