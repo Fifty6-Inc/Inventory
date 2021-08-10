2960189aacdee1be9965086baf48c011d61b30b3
Mikael Weiss <Campingmikael@icloud.com>
2021-08-10 16:11:48 -0600

    Removed unneeded test files

---
+++ InventoryTests/TestSupport.swift
@@ -0,0 +1,13 @@
+//
+//  TestSupport.swift
+//  InventoryTests
+//
+//  Created by Mikael Weiss on 8/10/21.
+//
+
+import Foundation
+@testable import Inventory
+
+enum TestError: Error {
+    case error
+}
